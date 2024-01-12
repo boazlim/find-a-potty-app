09a5d1fd84392b9f26b2001eaf1f7a8748f005f4
boazlim <boslim.646@gmail.com>
2024-01-12 09:03:54 -0500

    startscreen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,11 +1,9 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:travel_routes/location.dart';
+import 'package:travel_routes/user_screen.dart'; // Import the user screen
 
 class StartScreen extends StatefulWidget {
-  const StartScreen ({super.key});
+  const StartScreen({Key? key}) : super(key: key);
 
   @override
   StartScreenState createState() => StartScreenState();
@@ -14,19 +12,51 @@ class StartScreen extends StatefulWidget {
 class StartScreenState extends State<StartScreen> {
   late TextEditingController usernameController;
 
-  @override 
-
+  @override
   void initState() {
     super.initState();
-    usernameController = TextEditingController(text: widget.Location.getTitle());
+    usernameController = TextEditingController();
   }
 
+  void _navigateToUserScreen(String username) {
+    User user = User(username);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const UserScreen(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TextField(
-        controller: usernameController,
-        decoration: const InputDecoration(labelText: 'Enter a Username'),
-      )
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(labelText: 'Enter a Username'),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String username = usernameController.text.trim();
+                if (username.isNotEmpty) {
+                  _navigateToUserScreen(username);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please enter a username')),
+                  );
+                }
+              },
+              child: const Text('Log In'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
